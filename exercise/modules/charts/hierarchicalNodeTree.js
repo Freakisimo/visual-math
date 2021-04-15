837d554df3e4164c3aeec45b89cfe1f1c2165e6d
dante7 <dante.arkade7@gmail.com>
2021-04-15 01:20:18 -0500

    Change tree structure to vertical and calculate separation between nodes and svg height

--- exercise/modules/charts/hierarchicalNodeTree.js
+++ exercise/modules/charts/hierarchicalNodeTree.js
@@ -6,17 +6,17 @@ class HierarchicalNodeTree {
 
     constructor(args) {
         const margin = {
-            top: 10,
-            right: 100,
+            top: 50,
+            right: 50,
             bottom: 50,
-            left: 100
+            left: 50
         };
-        const width = 600, height = 500;
+        const width = 600, height = 800;
 
         this.selector = args.selector || "#chart-nodeTree"
         this.margin = args.margin || complete_json(margin, args.margin);
         this.width = args.width || width - this.margin.left - this.margin.right;
-        this.height = args.height || height - this.margin.left - this.margin.right;
+        this.height = args.height || height - this.margin.top - this.margin.bottom;
         this.element = document.querySelector(this.selector)
         this.radius = Math.min(this.width, this.height) / 2;
         if (!args.data) {
@@ -29,7 +29,7 @@ class HierarchicalNodeTree {
         } catch (error) {
             console.error(error);
         }
-        console.log(this.root)
+        // console.log(this.root)
 
     }
     draw() {
@@ -43,33 +43,26 @@ class HierarchicalNodeTree {
         // we'll actually be appending to a <g> element
         this.plot = svg.append('g')
             .attr("font-family", "sans-serif")
-            .attr("font-size", 10)
-            .attr('transform', `translate(${this.width/2},${(this.height/2)+10})`);
+            .attr("font-size", 15)
+            .attr('transform', `translate(${this.width},${(this.height/4)+10})`);
 
         this.createChart();
 
     }
 
+    // Calculate position for each node
     treeData(data, width) {
         const root = d3.stratify()
             .id(d => d.id )
             .parentId(d => d.parent)
             (data)
-        root.dx = 10;
-        root.dy = width / (root.height + 1);
+        root.dx = (this.width / 2) / (root.height + 1);
+        root.dy = (this.height / 2) / (root.height + 1);
         return d3.tree().nodeSize([root.dx, root.dy])(root);
     }
 
     createChart() {
         let _this = this;
-
-        let x0 = Infinity;
-        let x1 = -x0;
-        
-        _this.root.each(d => {
-            if (d.x > x1) x1 = d.x;
-            if (d.x < x0) x0 = d.x;
-        });
 
         const link = _this.plot.append("g")
             .attr("fill", "none")
@@ -79,9 +72,9 @@ class HierarchicalNodeTree {
         .selectAll("path")
             .data(_this.root.links())
             .join("path")
-            .attr("d", d3.linkHorizontal()
-                .x(d => d.y)
-                .y(d => d.x));
+            .attr("d", d3.linkVertical()
+                .x(d => d.x)
+                .y(d => d.y));
 
         const node = _this.plot.append("g")
             .attr("stroke-linejoin", "round")
@@ -89,16 +82,16 @@ class HierarchicalNodeTree {
             .selectAll("g")
             .data(_this.root.descendants())
             .join("g")
-            .attr("transform", d => `translate(${d.y},${d.x})`);
+            .attr("transform", d => `translate(${d.x},${d.y})`);
         
         node.append("circle")
-            .attr("fill", d => d.children ? "#555" : "#999")
-            .attr("r", 2.5);
+            .attr("fill", d => d.children ? "#427BF5" : "#DE5B37")
+            .attr("r", 5);
         
         node.append("text")
-            .attr("dy", "0.31em")
-            .attr("x", d => d.children ? -6 : 6)
-            .attr("text-anchor", d => d.children ? "end" : "start")
+            .attr("dy", "-0.8em")
+            .attr("x", d => d.children)
+            .attr("text-anchor", d => "middle")
             .text(d => d.data.value)
             .clone(true).lower()
             .attr("stroke", "white");
